fcc30d447b38f41b17163cb87a251d0e11c4a899
iochem <brendanunesdeandradeiochem@gmail.com>
2026-04-28 14:05:28 -0300

    first commit

---
+++ indy/src/main/java/com/telemetry/indyTelemetry/IndustryApplication.java
@@ -0,0 +1,12 @@
+package com.telemetry.indyTelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryApplication.class, args);
+	}
+}
