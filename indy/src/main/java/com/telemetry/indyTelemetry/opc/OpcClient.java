dd0547fa57b982734d7f383ddeb2d1be3f39e56c
iochem <brendanunesdeandradeiochem@gmail.com>
2026-05-04 09:37:23 -0300

    feat: create OpcClient class

---
+++ indy/src/main/java/com/telemetry/indyTelemetry/opc/OpcClient.java
@@ -0,0 +1,55 @@
+package com.telemetry.indyTelemetry.opc;
+
+import com.telemetry.indyTelemetry.config.AssetConfig;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
+public class OpcClient { // Handles operational state polling and telemetry subscriptions
+
+    private final AssetConfig config;
+    private OpcUaClient client;
+    //private OpcTelemetrySubscriber subscriber;
+    //private volatile boolean sessionActive = false;
+    private static final Logger log = LoggerFactory.getLogger(OpcClient.class);
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+
+    public void start() {
+        scheduler.scheduleWithFixedDelay(this::ensureSession, 0, 10, TimeUnit.SECONDS);
+    }
+
+    private synchronized void ensureSession() {
+
+        try{
+            client = client.create(config.getEndpoint()); // create
+            client.connect().get(15, TimeUnit.SECONDS);
+
+            startStatusMonitor();
+            startMonitorSubscription();
+
+        } catch (Exception e) {
+            log.error("OPC UA session unavailable for asset: {} | {}", config.getAssetName(), e.getMessage(), e);
+        }
+    }
+
+
+    private void startStatusMonitor(){
+        log.info("Hearbet started for: {} ", config.getAssetName());
+
+
+    }
+
+    private void startMonitorSubscription(){
+        log.info("Subscription started for: {} ", config.getAssetName());
+
+    }
+
+}
+
