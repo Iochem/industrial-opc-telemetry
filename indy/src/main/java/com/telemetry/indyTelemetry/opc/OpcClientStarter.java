bef02fdc3962f3b364d5b95f28b2072287e52f5b
iochem <brendanunesdeandradeiochem@gmail.com>
2026-05-04 09:38:13 -0300

    feat: create OpcClientStarter class

---
+++ indy/src/main/java/com/telemetry/indyTelemetry/opc/OpcClientStarter.java
@@ -0,0 +1,23 @@
+package com.telemetry.indyTelemetry.opc;
+
+
+import com.telemetry.indyTelemetry.config.AssetConfigFactory;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OpcClientStarter { //To start the client OPC
+
+    private final AssetConfigFactory machineConfigFactory;
+
+    @PostConstruct
+    public void start(){
+        machineConfigFactory.createAssents().forEach( assent -> {
+            new OpcClient(assent).start();
+        });
+
+    }
+
+}
