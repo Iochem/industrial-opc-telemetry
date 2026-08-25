5fd17af4184f66e0f96e4da7780ad695b33c51e2
iochem <brendanunesdeandradeiochem@gmail.com>
2026-08-25 12:07:12 -0300

    feat: add OPC UA event producer integration

--- indy/src/main/java/com/telemetry/indyTelemetry/infrastructure/opc/OpcClientStarter.java
+++ indy/src/main/java/com/telemetry/indyTelemetry/infrastructure/opc/OpcClientStarter.java
@@ -1,7 +1,6 @@
 package com.telemetry.indyTelemetry.infrastructure.opc;
 
-
-
+import com.telemetry.indyTelemetry.infrastructure.apacheKafka.service.AssetProducer;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,12 +11,12 @@ public class OpcClientStarter { //To start the client OPC
 
     private final com.telemetry.indyTelemetry.infrastructure.configTags.AssetConfigFactory machineConfigFactory;
 
+    private final AssetProducer producer;
+
     @PostConstruct
     public void start(){
         machineConfigFactory.createAssets().forEach( assent -> {
-            new OpcClient(assent).start();
+            new OpcClient(assent, producer).start();
         });
-
     }
-
 }
